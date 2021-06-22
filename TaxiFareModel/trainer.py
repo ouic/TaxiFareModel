e9818196432b8e05bf7238f3cb934c9752c4349d
Eric Pinto <eric.pinto@hotmail.fr>
2021-06-22 14:08:26 +0200

    first push

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,4 +1,14 @@
 # imports
+import pandas as pd
+from TaxiFareModel.data import *
+from TaxiFareModel.utils import *
+from TaxiFareModel.encoders import *
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
 
 class Trainer():
     def __init__(self, X, y):
@@ -12,22 +22,47 @@ class Trainer():
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
-        pass
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+        self.pipeline = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
 
     def run(self):
         """set and train the pipeline"""
-        pass
+        self.set_pipeline()
+        self.pipeline.fit(self.X, self.y)
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
-        pass
+        y_pred = self.pipeline.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+        return rmse
 
 
 if __name__ == "__main__":
     # get data
+    df = get_data()
     # clean data
+    df = clean_data(df)
     # set X and y
+    y = df.pop("fare_amount")
+    X = df
     # hold out
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     # train
+    trainer = Trainer(X_train, y_train)
+    trainer.run()
     # evaluate
-    print('TODO')
+    print(f'RMSE = {trainer.evaluate(X_test, y_test)}')
